f1b946c9a04e041f4c9f7db598997c8fdf7a57e9
Mgalinri <galindom@jbu.edu>
2025-03-15 16:22:50 -0500

    The router was added

--- finance_bro_tool/src/App.js
+++ finance_bro_tool/src/App.js
@@ -1,28 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter, Routes, Route } from "react-router";
+import Login from './pages/login';
 
 function App() {
-  return (   <div className="flex flex-col items-center justify-center  h-screen w-100">
-    <header>
-       <h1 className="text-primary font-black text-xl">The Finance Bro Tool</h1>
-    </header>
-    <div className="flex  flex-col items-center justify-center w-[576px] p-4">
-    <div className="flex flex-col items-center justify-center w-1/2 p-4">
-     <label className=" text-secondary font-bold self-start" for="email">Email</label>
-     <input type="text" id ="email" placeholder="hi@gmail.com" className="border w-full  border-gray-300 p-2 rounded-lg" />
-     </div>
-     <div className="flex flex-col items-center w-1/2 justify-center p-4">
-     <label className="  text-secondary font-bold self-start" for="password">Password</label>
-     <input type="text" id ="password" placeholder="******" className="border  w-full border-gray-300 p-2 rounded-lg" />
-     </div>
-     <div className="flex flex-col items-center w-1/2 justify-center p-4 gap-2">
-     <button className="bg-secondary font-bold text-white w-full p-2 rounded-lg">Sign In</button>
-     <button className="bg-secondary   font-bold text-white w-full p-2 rounded-lg">Sign Up</button>
-     </div>
-     </div>
- </div>
-  
+  return (   
+ <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />}>
+        </Route>
+      </Routes>
+    </BrowserRouter>
  
  
   );
--- finance_bro_tool/src/pages/login.js
+++ finance_bro_tool/src/pages/login.js
@@ -1,30 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
 import { BrowserRouter, Routes, Route } from "react-router";
 
-function App() {
+function Login() {
   return (
     <div className="flex flex-col items-center justify-center  h-screen w-100">
-       <header>
-          <h1 className="text-primary font-black">The Finance Bro Tool</h1>
-       </header>
-       <div className="flex  flex-col items-center justify-center w-[576px] p-4">
-       <div className="flex flex-col items-center justify-center w-1/2 p-4">
-        <label className=" text-secondary font-bold self-start" for="email">Email</label>
-        <input type="text" id ="email" placeholder="hi@gmail.com" className="border w-full  border-gray-300 p-2 rounded-lg" />
-        </div>
-        <div className="flex flex-col items-center w-1/2 justify-center p-4">
-        <label className="  text-secondary font-bold self-start" for="password">Password</label>
-        <input type="text" id ="password" placeholder="******" className="border  w-full border-gray-300 p-2 rounded-lg" />
-        </div>
-        <div className="flex flex-col items-center w-1/2 justify-center p-4 gap-2">
-        <button className="bg-secondary font-bold text-white w-full p-2 rounded-lg">Sign In</button>
-        <button className="bg-secondary   font-bold text-white w-full p-2 rounded-lg">Sign Up</button>
-        </div>
-        </div>
-    </div>
+    <header>
+       <h1 className="text-primary font-black text-xl">The Finance Bro Tool</h1>
+    </header>
+    <div className="flex  flex-col items-center justify-center w-[576px] p-4">
+    <div className="flex flex-col items-center justify-center w-1/2 p-4">
+     <label className=" text-secondary font-bold self-start" for="email">Email</label>
+     <input type="text" id ="email" placeholder="hi@gmail.com" className="border w-full  border-gray-300 p-2 rounded-lg" />
+     </div>
+     <div className="flex flex-col items-center w-1/2 justify-center p-4">
+     <label className="  text-secondary font-bold self-start" for="password">Password</label>
+     <input type="text" id ="password" placeholder="******" className="border  w-full border-gray-300 p-2 rounded-lg" />
+     </div>
+     <div className="flex flex-col items-center w-1/2 justify-center p-4 gap-2">
+     <button className="bg-secondary font-bold text-white w-full p-2 rounded-lg">Sign In</button>
+     <button className="bg-secondary   font-bold text-white w-full p-2 rounded-lg">Sign Up</button>
+     </div>
+     </div>
+ </div>
  
   );
 }
 
-export default App;
+export default Login;
